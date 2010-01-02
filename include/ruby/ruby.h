077c51c1548e634a41ce4b0fe27b1322ad5dcf9d
Yuki Sonoda (Yugui) <yugui@yugui.jp>
2010-01-02 23:11:05 +0900

    fix for gcc 2.x

--- include/ruby/ruby.h
+++ include/ruby/ruby.h
@@ -45,6 +45,9 @@ extern "C" {
 # if __GNUC__ >= 3
 #  define LIKELY(x)   (__builtin_expect((x), 1))
 #  define UNLIKELY(x) (__builtin_expect((x), 0))
+# else
+#  define LIKELY(x)   (x)
+#  define UNLIKELY(x) (x)
 # endif
 #else
 # define PRINTF_ARGS(decl, string_index, first_to_check) decl
