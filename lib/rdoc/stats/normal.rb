ruby/rdoc

commit 8cfa11d6f1cc7e74e45e42fd8d1d76c5a0646a6e

Use IO.console.winsize instead of IO.console_size for JRuby 9k

lib/rdoc/stats/normal.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: false
-require 'io/console/size'
+require 'io/console'
 
 ##
 # Stats printer that prints just the files being documented with a progress
@@ -23,7 +23,7 @@ class RDoc::Stats::Normal < RDoc::Stats::Quiet
 
     # Print a progress bar, but make sure it fits on a single line. Filename
     # will be truncated if necessary.
-    terminal_width = IO.console_size[1].to_i.nonzero? || 80
+    terminal_width = IO.console.winsize[1].to_i.nonzero? || 80
     max_filename_size = terminal_width - progress_bar.size
 
     if filename.size > max_filename_size then

yugui/ruby

commit 4a04e25c368445d49ceec01cbadb6781e2ec3077

Partly reverted r59845.
  All of environments on Ruby CI are fails rdoc generation.
  It may be caused by
  https://github.com/ruby/rdoc/commit/8cfa11d6f1cc7e74e45e42fd8d1d76c5a0646a6e

git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@59856 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

lib/rdoc/stats/normal.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: false
-require 'io/console'
+require 'io/console/size'
 
 ##
 # Stats printer that prints just the files being documented with a progress
@@ -23,7 +23,7 @@ class RDoc::Stats::Normal < RDoc::Stats::Quiet
 
     # Print a progress bar, but make sure it fits on a single line. Filename
     # will be truncated if necessary.
-    terminal_width = IO.console.winsize[1].to_i.nonzero? || 80
+    terminal_width = IO.console_size[1].to_i.nonzero? || 80
     max_filename_size = terminal_width - progress_bar.size
 
     if filename.size > max_filename_size then